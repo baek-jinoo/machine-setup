d89b7f61bd1e3c2166874789ca941eaf86d9b3c7
Jin <jin@Jinwoos-MacBook-Pro-2.local>
2015-05-18 23:10:33 -0700

    Bashrc setup

---
+++ scripts/bashrc_setup.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+require_relative 'shared'
+
+def install_bashrc
+  settings_destination_full_path = "#{ENV['HOME']}/.bashrc"
+  colorize_log 'Attempting to install bashrc2'
+  if !File.symlink?(settings_destination_full_path) && !File.exists?(settings_destination_full_path) then
+    colorize_log 'Installing bashrc'
+    shell_cmd "ln -s #{File.expand_path(File.dirname(__FILE__))}/../artifacts/bash/bashrc #{settings_destination_full_path}" 
+  else
+    colorize_log 'bashrc already installed'
+  end
+end
+
+install_bashrc
